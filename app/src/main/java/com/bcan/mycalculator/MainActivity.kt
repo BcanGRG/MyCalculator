e66e6d42fe29b5e98a0c725c7ff0ae570d6adc61
Burak Can <BurakCan_GRG@hotmail.com>
2022-07-12 13:04:49 +0300

    Operatörlere uygun işlem fonksiyonları yazıldı

--- app/src/main/java/com/bcan/mycalculator/MainActivity.kt
+++ app/src/main/java/com/bcan/mycalculator/MainActivity.kt
@@ -37,14 +37,76 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onOperator ( view: View){
+    fun onOperator(view: View) {
         tvInput?.text?.let {
-            if(isLastNumeric && !isOperatorAdded(it.toString())){
+            if (isLastNumeric && !isOperatorAdded(it.toString())) {
                 tvInput?.append((view as Button).text)
                 isLastNumeric = false
                 isLastDot = false
             }
         }
+    }
+
+    fun onEqual(view: View) {
+        if (isLastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+            try {
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if (tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }else if (tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeZeroAfterDot( (one.toDouble() + two.toDouble()).toString())
+                } else if (tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }else if (tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                }
+
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot (result : String) : String{
+        var value = result
+        if (result.contains(".0")){
+            value = result.substring(0,result.length -2)
+
+        }
+        return value
     }
 
     private fun isOperatorAdded(value: String): Boolean {
